87e0fa5d6d9b83835bf6f16343e58594e953a47b
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-10-04 11:11:15 +0330

    chore: delete date filter

--- config/date.js
+++
@@ -1,7 +0,0 @@
-var monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-function date(content) {
-  return `${monthName[content.getMonth()]} ${content.getDate()}, ${content.getFullYear()}`;
-}
-
-module.exports = {date};
