28fd75708d96f5496484893ebf8b6f07c1649d2e
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-10-10 23:48:50 +0330

    feat: add alpine

--- site/_ts/common.ts
+++ site/_ts/common.ts
@@ -1,3 +1,5 @@
+import alpine from 'alpinejs';
+
 import './lib/banner.js';
 import './lib/header.js';
 import {logger} from './lib/logger.js';
@@ -5,3 +7,5 @@ import './lib/quick-link.js';
 import './lib/register-service-worker.js';
 
 logger.logModule?.('common');
+
+alpine.start();
