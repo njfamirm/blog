d91313f16bf52c500d8dde57fc3e5ae29c1f6f7f
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-20 23:52:28 +0330

    feat: add service worker

--- site/_ts/main.ts
+++ site/_ts/main.ts
@@ -3,7 +3,7 @@ import alpine from 'alpinejs';
 import './lib/header.js';
 import {logger} from './lib/logger.js';
 import './lib/quick-link.js';
-import './lib/register-service-worker.js';
+import './lib/service-worker.js';
 
 logger.banner?.('Alwatr 12fy');
 logger.logModule?.('main');
