923326aeb5cfb1b6665b86f1ebe04ebcf4d7c283
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-21 22:57:22 +0330

    feat(blog): use giscuss as comment system

---
+++ site/_ts/lib/comment.ts
@@ -0,0 +1,19 @@
+export function loadComments() {
+  const script = document.createElement('script');
+  script.src = "https://giscus.app/client.js";
+  script.dataset.repo = "njfamirm/blog";
+  script.dataset.repoId = "R_kgDOKGE2Wg";
+  script.dataset.category = "Blog";
+  script.dataset.categoryId = "DIC_kwDOKGE2Ws4CZie1";
+  script.dataset.mapping = "title";
+  script.dataset.strict = "0";
+  script.dataset.reactionsEnabled = "1";
+  script.dataset.emitMetadata = "0";
+  script.dataset.inputPosition = "top";
+  script.dataset.theme = "noborder_dark";
+  script.dataset.lang = "en";
+  script.crossOrigin = "anonymous";
+  script.defer = true;
+  script.async = true;
+  document.body.appendChild(script);
+}
--- site/_ts/main.ts
+++ site/_ts/main.ts
@@ -1,5 +1,6 @@
 import alpine from 'alpinejs';
 
+import {loadComments} from './lib/comment.js';
 import './lib/header.js';
 import {logger} from './lib/logger.js';
 import './lib/service-worker.js';
@@ -8,3 +9,5 @@ logger.banner?.('Alwatr 12fy');
 logger.logModule?.('main');
 
 alpine.start();
+
+window.onload = loadComments;
