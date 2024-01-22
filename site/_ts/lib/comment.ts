749672dc37e40e72973b75bd77e410a0cd3febea
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-22 08:39:21 +0330

    fix(blog): prevent loading comment in all page

--- site/_ts/lib/comment.ts
+++ site/_ts/lib/comment.ts
@@ -1,18 +1,23 @@
+import {logger} from './logger.js';
+
 export function loadComments() {
+  if (document.querySelector('.giscus') == null) return;
+  logger.logMethod?.('loadComments');
+
   const script = document.createElement('script');
-  script.src = "https://giscus.app/client.js";
-  script.dataset.repo = "njfamirm/blog";
-  script.dataset.repoId = "R_kgDOKGE2Wg";
-  script.dataset.category = "Blog";
-  script.dataset.categoryId = "DIC_kwDOKGE2Ws4CZie1";
-  script.dataset.mapping = "title";
-  script.dataset.strict = "0";
-  script.dataset.reactionsEnabled = "1";
-  script.dataset.emitMetadata = "0";
-  script.dataset.inputPosition = "top";
-  script.dataset.theme = "noborder_dark";
-  script.dataset.lang = "en";
-  script.crossOrigin = "anonymous";
+  script.src = 'https://giscus.app/client.js';
+  script.dataset.repo = 'njfamirm/blog';
+  script.dataset.repoId = 'R_kgDOKGE2Wg';
+  script.dataset.category = 'Blog';
+  script.dataset.categoryId = 'DIC_kwDOKGE2Ws4CZie1';
+  script.dataset.mapping = 'title';
+  script.dataset.strict = '0';
+  script.dataset.reactionsEnabled = '1';
+  script.dataset.emitMetadata = '0';
+  script.dataset.inputPosition = 'top';
+  script.dataset.theme = 'noborder_dark';
+  script.dataset.lang = 'en';
+  script.crossOrigin = 'anonymous';
   script.defer = true;
   script.async = true;
   document.body.appendChild(script);
